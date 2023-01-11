70294957cd03a19fd9b9ab4f338c68f622b7065b
kanchanajz <it21109508@my.sliit.lk>
2023-01-11 14:53:58 +0530

    First Commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.menu');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+    menu.classList.toggle('move');
+    bell.classList.remove('active');
+
+}
+
+let bell = document.querySelector('.notifications');
+
+document.querySelector('#bell-icon').onclick = () => {
+    bell.classList.toggle('active');
+}
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+  });
+
+window.onscroll = function() {mufunction()};
+
+function mufunction(){
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll/height) * 100;
+  document.getElementById('scroll-bar').style.width =scrolled + '%';
+}
+
+
+
